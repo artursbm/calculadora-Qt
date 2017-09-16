2f5f09bb697ac8a840641e6081b645a11f6b72d1
artursbm <artursbmello@gmail.com>
2017-09-16 17:59:50 -0300

    alterações iniciais no modo normal (Apenas interface, preciso descobrir como fazer SIGNAL/SLOT) da calculadora

---
+++ source/calculator/calculator.cpp
@@ -0,0 +1,55 @@
+#include "calculator.h"
+
+Calculator::Calculator(QObject *parent) :
+    QObject(parent), m_lastResult(0), m_currentNumber(0), m_isAdding(true), m_isMultiplying(true), m_isDividing(true)
+{
+}
+
+void Calculator::numEntered(int num)
+{
+    m_currentNumber = m_currentNumber*10 + num;
+    emit displayChanged(QString::number(m_currentNumber));
+}
+
+void Calculator::clear()
+{
+    m_currentNumber = 0;
+    emit displayChanged(QString::number(m_currentNumber));
+}
+
+void Calculator::allClear()
+{
+    m_currentNumber = 0;
+    m_lastResult = 0;
+    emit displayChanged(QString::number(m_currentNumber));
+}
+
+void Calculator::additionMode()
+{
+    calculate();
+    m_isAdding = true;
+
+}
+
+void Calculator::subtractionMode()
+{
+    calculate();
+    m_isAdding = false;
+}
+
+void Calculator::multiplicationMode() {
+    calculate();
+    m_isMultiplying = true;
+}
+
+void Calculator::divisionMode() {
+    calculate();
+    m_isDividing = true;
+}
+
+void Calculator::calculate()
+{
+    m_lastResult += m_isAdding?m_currentNumber:-m_currentNumber;
+    m_currentNumber = 0;
+    emit displayChanged(QString::number(m_lastResult));
+}
---
+++ source/calculator/calculator.h
@@ -0,0 +1,36 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QObject>
+
+class Calculator : public QObject
+{
+Q_OBJECT
+public:
+    explicit Calculator(QObject *parent = 0);
+
+public slots:
+    void numEntered(int);
+
+    void clear();
+    void allClear();
+
+    void additionMode();
+    void subtractionMode();
+    void multiplicationMode();
+    void divisionMode();
+
+    void calculate();
+
+signals:
+    void displayChanged(QString);
+
+private:
+    int m_lastResult;
+    int m_currentNumber;
+    bool m_isAdding;
+    bool m_isMultiplying;
+    bool m_isDividing;
+};
+
+#endif // CALCULATOR_H
